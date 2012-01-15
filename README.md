b402e9fb5b1642d9a6d5b879214c8a8519761a91
Christopher J. Madsen <perl@cjmweb.net>
2012-01-14 20:30:51 -0600

    IO-HTML created by Dist::Zilla

---
+++ README.md
@@ -0,0 +1,16 @@
+IO-HTML
+=======
+
+IO-HTML is a Perl module that NEEDS A DESCRIPTION.
+
+This is a Git repository where development of IO-HTML takes place.  For more information, visit [IO-HTML on CPAN](http://search.cpan.org/dist/IO-HTML/).
+
+
+
+Copyright and License
+=====================
+
+This software is copyright (c) 2012 by Christopher J. Madsen.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
---
+++ lib/IO/HTML.pm
@@ -0,0 +1,38 @@
+#---------------------------------------------------------------------
+package IO::HTML;
+#
+# Copyright 2012 Christopher J. Madsen
+#
+# Author: Christopher J. Madsen <perl@cjmweb.net>
+# Created: 14 Jan 2012
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the
+# GNU General Public License or the Artistic License for more details.
+#
+# ABSTRACT:
+#---------------------------------------------------------------------
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+# This file is part of {{$dist}} {{$dist_version}} ({{$date}})
+
+#=====================================================================
+
+#=====================================================================
+# Package Return Value:
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  use IO::HTML;
