47536c50648b368cce02ab479fd64ab48fd5d4c4
Christopher J. Madsen <perl@cjmweb.net>
2015-09-26 16:05:50 -0500

    Indicate trial releases

--- tools/loom.pl
+++ tools/loom.pl
@@ -11,7 +11,7 @@ use warnings;
   sort_method => 1,
   # This template will be filled in by TemplateCJM:
   version_desc => <<'END VERSION',
-This document describes version {{$version}} of
+This document describes version {{$version}}{{ $zilla->is_trial ? ' (TRIAL)' : ''}} of
 {{$module}}, released {{$date}}.
 END VERSION
 #as part of {{$dist}} version {{$dist_version}}.
